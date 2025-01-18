8b3313c91d5a668103d26710ea10788dae3c132f
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-01-19 00:04:51 +0530

    Add user LED configuration comment in main.c for clarity

--- 2_gpio_output/Src/main.c
+++ 2_gpio_output/Src/main.c
@@ -1,5 +1,8 @@
-#include "stm32f4xx.h"
-#include "stm32f411xe.h"
+/*STM32F411CEU6-WeAct Blackphill User LED Configuration 3
+  BareMetal Programming with STM Header Files(https://github.com/STMicroelectronics/STM32CubeF4)*/
+
+#include "stm32f4xx.h" //Include the STM32F4xx header file(Standard peripheral library)
+#include "stm32f411xe.h"//Include the STM32F411 header file(Standard peripheral library)
  
 #define GPIOCEN     (1UL << 2)
 #define PIN13       (1UL << 13)
