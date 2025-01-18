a9875711a265223b53580d5e0dfe9cd8355d7375
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-01-18 23:49:34 +0530

    Add main.c implementation for GPIO LED toggle functionality in 2 _gpio_output

--- 2_gpio_output/Src/main.c
+++ 2_gpio_output/Src/main.c
@@ -1,5 +1,18 @@
 #include "stm32f4xx.h"
+#include "stm32f411xe.h"
+ 
+#define GPIOCEN     (1UL << 2)
+#define PIN13       (1UL << 13)
+#define LED_PIN     PIN13
 
+int main(void){
+    RCC ->AHB1ENR |= GPIOCEN; // Enable clock for GPIOC
+    GPIOC ->MODER |= (1 << 26); // Set pin 13 as output
 
-
-
+    while (1)
+    {
+        GPIOC ->ODR ^= LED_PIN; // Toggle pin 13
+        for (int i = 0; i < 100000; i++); // Delay
+    }
+    
+}
