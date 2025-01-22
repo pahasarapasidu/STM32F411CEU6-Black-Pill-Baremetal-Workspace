c7e5d123d92d47b8e1d585a9bf9e0b9d02422d5d
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-01-22 23:54:38 +0530

    Update UART baud rate to 115200

--- 8_uart_rx/Src/uart.c
+++ 8_uart_rx/Src/uart.c
@@ -15,7 +15,7 @@
 #define SYS_FREQ    16000000U
 #define APB1_CLK    SYS_FREQ
 
-#define UART_BAUDRATE 9600U
+#define UART_BAUDRATE 115200U
 
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t Baudrate);
 static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t Baudrate);
