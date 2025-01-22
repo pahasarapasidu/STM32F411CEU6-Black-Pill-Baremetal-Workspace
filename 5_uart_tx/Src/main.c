80e7ed2ace426dcaf990f9edf36481c73b4979bd
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-01-22 17:06:57 +0530

    Refactor UART configuration to use USART2 instead of USART6 and update baud rate to 9600

--- 5_uart_tx/Src/main.c
+++ 5_uart_tx/Src/main.c
@@ -2,79 +2,88 @@
   BareMetal Programming with STM Header Files(https://github.com/STMicroelectronics/STM32CubeF4)
  */
 
+#include <stdint.h> //Include the standard integer header file
 #include "stm32f4xx.h" //Include the STM32F4xx header file(Standard peripheral library)
 #include "stm32f411xe.h"//Include the STM32F411 header file(Standard peripheral library)
  
 #define GPIOAEN     (1UL << 0)
-#define UART6EN     (1UL << 5)
+#define UART2EN     (1UL << 17)
 
 #define CR1_TE      (1UL << 3)
 #define CR1_UE      (1UL << 13) 
 #define SR_TXE      (1UL << 7)
 
 #define SYS_FREQ    16000000U
-#define APB2_CLK    SYS_FREQ
+#define APB1_CLK    SYS_FREQ
 
-#define UART_BAUDRATE 115200U
+#define UART_BAUDRATE 9600U
 
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t Baudrate);
 static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t Baudrate);
 
-void uart6_tx_init(void);
-void usart6_write(int ch);
+void uart2_tx_init(void);
+void usart2_write(int ch);
 
 int main(void){
 
-  uart6_tx_init();
+  uart2_tx_init();
 
 
-    while (1)
-    {
-        usart6_write('S'); 
-    }
+  while (1)
+  {
+    usart2_write('Y');
+
+    for(volatile int i = 0; i < 1000000; i++);
+  }
     
 }
 
-void uart6_tx_init(void)
+void uart2_tx_init(void)
 {
     /**************Configure uart gpio pin**************/
     /*Enable clock access to gpioA*/
     RCC ->AHB1ENR |= GPIOAEN;
-    /*Set PA11 mode to alternate function mode*/
-    GPIOA ->MODER |= (1UL << 23);
-    GPIOA ->MODER &= ~(1UL << 22);
+    /*Set PA2 mode to alternate function mode*/
+    GPIOA ->MODER |= (1UL << 5);
+    GPIOA ->MODER &= ~(1UL << 4);
 
-    /*Ser PA11 alternate function type to UART_TX (AF08)*/
-    GPIOA ->AFR[1] |= (1UL << 15);
-    GPIOA ->AFR[1] &= ~(1UL << 14);
-    GPIOA ->AFR[1] &= ~(1UL << 13);
-    GPIOA ->AFR[1] &= ~(1UL << 12);
+     /*Ser PA2 alternate function type to UART_TX (AF07)*/
+     GPIOA ->AFR[0] &= ~(1UL << 11);
+     GPIOA ->AFR[0] |= (1UL << 10);
+     GPIOA ->AFR[0] |= (1UL << 9);
+     GPIOA ->AFR[0] |= (1UL << 8);
+
+//    /*Set PA2 alternate function type to UART_TX (AF07)*/
+//    // Clear the bits first
+//    GPIOA->AFR[0] &= ~(0xFU << 8);  // Clear bits 8-11
+//    // Set to AF7 (0111)
+//    GPIOA->AFR[0] |= (7U << 8);     // Set to AF7
 
     /**************Configure uart module**************/
-    /*Enable clock access to uart6*/
-    RCC ->APB2ENR |= UART6EN;
+    /*Enable clock access to uart2*/
+    RCC ->APB1ENR |= UART2EN;
 
     /*Configure baudrate*/
-    uart_set_baudrate(USART6, APB2_CLK, UART_BAUDRATE);
+    uart_set_baudrate(USART2, APB1_CLK, UART_BAUDRATE);
 
     /*Configure the transfer direction*/
-    USART6 ->CR1 = CR1_TE;
+    USART2 ->CR1 = CR1_TE;
 
     /*Enable uart module*/
-    USART6 ->CR1 |= CR1_UE;
+    USART2 ->CR1 |= CR1_UE;
 
 }
 
 
-void usart6_write(int ch)
+void usart2_write(int ch)
 {
     /*****Write Data to transmit*****/
-    
+
   /*Make sure the transmit data register is empty*/
-  while(!(USART6 ->SR & SR_TXE)){}
+  while(!(USART2 ->SR & SR_TXE)){}
 
   /*Write to transmit data register*/ 
-  USART6 ->DR = (ch & 0xFF);
+  USART2 ->DR = (ch & 0xFF);
 
 
 
