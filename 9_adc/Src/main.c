027294986c7c80e9074191fa9810be84167d4115
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-01-23 13:57:37 +0530

    Add initial project files for STM32F411CEU6 adc configuration

---
+++ 9_adc/Src/main.c
@@ -0,0 +1,11 @@
+/*STM32F411CEU6-WeAct Blackpill UART_RXTX Configuration   
+  BareMetal Programming with STM Header Files(https://github.com/STMicroelectronics/STM32CubeF4)
+ */
+
+#include <stdio.h>//Include the standard input/output header file
+#include <stdint.h> //Include the standard integer header file
+#include "stm32f4xx.h" //Include the STM32F4xx header file(Standard peripheral library)
+#include "stm32f411xe.h"//Include the STM32F411 header file(Standard peripheral library)
+#include "uart.h"//Include the UART header file
+
+
