931d0847f320abcf54476a7a02acc1570c2deafc
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-01-26 23:59:39 +0530

    Implement ADC and UART functionality for STM32F411CEU6: add adc.h, uart.h, adc.c, and main.c files

---
+++ 10_adc_continous_conversion/Src/main.c
@@ -0,0 +1,33 @@
+/*STM32F411CEU6-WeAct Blackpill ADC Configuration (Single mode)
+  BareMetal Programming with STM Header Files(https://github.com/STMicroelectronics/STM32CubeF4)
+ */
+
+#include <stdio.h>//Include the standard input/output header file
+#include <stdint.h> //Include the standard integer header file
+#include "stm32f4xx.h" //Include the STM32F4xx header file(Standard peripheral library)
+#include "stm32f411xe.h"//Include the STM32F411 header file(Standard peripheral library)
+#include "uart.h"//Include the UART header file
+#include "adc.h"//Include the ADC header file
+
+uint32_t sensor_value;
+
+int main(void){
+
+  uart2_rxtx_init();
+  pa1_adc_init();
+
+  
+  while (1)
+  {
+    start_conversion();
+
+    sensor_value = adc_read();
+    printf("ADC Value: %d\n\r", (int)sensor_value);
+
+    for(volatile int i = 0; i < 100000; i++);
+
+  }
+
+}
+
+
