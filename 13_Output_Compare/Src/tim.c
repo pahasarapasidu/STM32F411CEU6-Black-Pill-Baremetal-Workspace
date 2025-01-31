7db271c5cf4260ca11d681fc2bd618ed76a938e4
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-01-31 23:10:12 +0530

    Edit TIM2 prescaler value

--- 13_Output_Compare/Src/tim.c
+++ 13_Output_Compare/Src/tim.c
@@ -33,7 +33,7 @@ void tim2_output_compare(void)
     RCC ->APB1ENR |= TIM2EN;
 
     /*Set prescaler value*/
-    TIM2 ->PSC = 1600 - 1; //16 000 000/1 600 = 10 000
+    TIM2 ->PSC = 800 - 1; //16 000 000/1 600 = 10 000
 
     /*Set auto-reload value*/
     TIM2 ->ARR = 10000 - 1; //10000/10 000 = 1Hz
