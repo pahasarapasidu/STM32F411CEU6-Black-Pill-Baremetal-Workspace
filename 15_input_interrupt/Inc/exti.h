2e9facaadcac3dbb4988ff70dba7973aeaf45aac
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-02-03 22:03:46 +0530

    Add EXTI initialization for button press on PB13

--- 15_input_interrupt/Inc/exti.h
+++ 15_input_interrupt/Inc/exti.h
@@ -1,13 +1,12 @@
-/*
- * exti.h
- *
- *  Created on: Feb 1, 2025
- *      Author: last_knight
- */
 
 #ifndef EXTI_H_
 #define EXTI_H_
 
+
+#include <stdio.h>//Include the standard input/output header file
+#include <stdint.h> //Include the standard integer header file
+#include "stm32f4xx.h" //Include the STM32F4xx header file(Standard peripheral library)
+#include "stm32f411xe.h"//Include the STM32F411 header file(Standard peripheral library)
 
 
 #endif /* EXTI_H_ */
--- 15_input_interrupt/Src/exti.c
+++ 15_input_interrupt/Src/exti.c
@@ -1,1 +1,20 @@
+#include "exti.h"
 
+#define GPIOBEN   (1UL << 1)
+
+void pb13_exti_init(void)
+{
+    /*Dsable global interrupt*/
+    __disable_irq();
+
+    /*Enable clock access for GPIOB */
+    RCC->AHB1ENR |= GPIOBEN;
+    
+    /*Enable clock access to SYSCFG*/
+    /*Select PORTA for EXTI13*/
+    /*Unmask EXTI13*/
+    /*Select falling edge trigger*/
+    /*Enable EXTI line in NVIC*/
+    /*Enable global interrupt*/
+    __enable_irq();
+}
