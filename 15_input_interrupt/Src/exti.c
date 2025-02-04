c804607446bef09ed5d19bbcd50cc1bb9f9056db
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-02-04 21:13:47 +0530

    config  PB13 as input

--- 15_input_interrupt/Src/exti.c
+++ 15_input_interrupt/Src/exti.c
@@ -1,6 +1,7 @@
 #include "exti.h"
 
 #define GPIOBEN   (1UL << 1)
+#define SYSCFGEN  (1UL << 14)
 
 void pb13_exti_init(void)
 {
@@ -9,10 +10,16 @@ void pb13_exti_init(void)
 
     /*Enable clock access for GPIOB */
     RCC->AHB1ENR |= GPIOBEN;
+
+    /*Set PB13 as input*/
+    GPIOB ->MODER &= ~(1UL <<26);
+    GPIOB ->MODER &= ~(1UL <<27);
     
     /*Enable clock access to SYSCFG*/
-    
+    RCC ->APB1ENR |= SYSCFGEN;
+
     /*Select PORTB for EXTI13*/
+
     /*Unmask EXTI13*/
     /*Select falling edge trigger*/
     /*Enable EXTI line in NVIC*/
