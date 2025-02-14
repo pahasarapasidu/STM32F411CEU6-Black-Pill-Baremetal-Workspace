3d202708e3026c166c9e22b23a793ac57d6745cd
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-02-14 21:58:44 +0530

    Add detailed header comment for systick.h file

--- 11_Systick_delay/Inc/systick.h
+++ 11_Systick_delay/Inc/systick.h
@@ -1,3 +1,20 @@
+/**
+  ******************************************************************************
+  * @file    systick.h
+  * @author  pahasarapasidu
+  * @brief   STM32F411CEU6-WeAct Blackphill ADC Configuration 
+  *
+  *          This file contains:
+  *           - Systick delay function prototypes
+  *          
+  *           
+  *
+  ******************************************************************************
+  *@attention All functions are tested with WeAct STM32F411CEU6 Blackpill board
+              Function definitions are in adc.c file
+  ******************************************************************************
+
+  */
 
 #ifndef SYSTICK_H_
 #define SYSTICK_H_
