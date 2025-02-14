0989b30a769db536cbb39ec7426412d918708024
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-02-14 22:05:12 +0530

    Enhance header comment in tim.h to provide detailed ADC configuration information

--- 12_Timers/Inc/tim.h
+++ 12_Timers/Inc/tim.h
@@ -1,9 +1,20 @@
-/*
- * tim.h
- *
- *  Created on: Jan 29, 2025
- *      Author: last_knight
- */
+/**
+  ******************************************************************************
+  * @file    tim.h
+  * @author  pahasarapasidu
+  * @brief   STM32F411CEU6-WeAct Blackphill ADC Configuration 
+  *
+  *          This file contains:
+  *           - TIM2 1Hz configuration
+  *          
+  *           
+  *
+  ******************************************************************************
+  *@attention All functions are tested with WeAct STM32F411CEU6 Blackpill board
+              Function definitions are in adc.c file
+  ******************************************************************************
+
+  */
 
 #ifndef TIM_H_
 #define TIM_H_
