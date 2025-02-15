cbc83e4edeb75ed80379028cd410488605a5c445
Pasidu Pahasara <pahasarapasidu@gmail.com>
2025-02-15 23:57:46 +0530

    Enhance header comment in tim.h to provide detailed TIM2 configuration and initialization information

--- 14_output_capture/Inc/tim.h
+++ 14_output_capture/Inc/tim.h
@@ -1,10 +1,27 @@
-/*
- * tim.h
- *
- *  Created on: Jan 29, 2025
- *      Author: last_knight
- */
+/**
+  ******************************************************************************
+  * @file    tim.h
+  * @author  pahasarapasidu
+  * @brief   STM32F411CEU6-WeAct Blackphill ADC Configuration 
+  *
+  *          This file contains:
+  *          **TIM2 1Hz configuration**
+  *           - PA5 AF mode
+  *           - TIM2 1Hz initialization
+  *          **TIM2 Output Compare configuration**
+  *           - PA5 AF mode
+  *           - TIM2 Output Compare initialization
+  * 
+  *  
+  *          
+  *           
+  *
+  ******************************************************************************
+  *@attention All functions are tested with WeAct STM32F411CEU6 Blackpill board
+              Function definitions are in tim.c file
+  ******************************************************************************
 
+  */
 #ifndef TIM_H_
 #define TIM_H_
 
--- 14_output_capture/Src/main.c
+++ 14_output_capture/Src/main.c
@@ -1,4 +1,4 @@
-/*STM32F411CEU6-WeAct Blackpill Delay Configuration (Using Systick Timer)
+/*STM32F411CEU6-WeAct Blackpill Capture the edge of the signal using TIM3
   BareMetal Programming with STM Header Files(https://github.com/STMicroelectronics/STM32CubeF4)
  */
 
